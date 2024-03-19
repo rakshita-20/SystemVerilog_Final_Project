676c30061fe3870299a719acd1e14a13bf3487b6
smabbas1 <150382180+smabbas1@users.noreply.github.com>
2024-03-19 14:19:00 -0700

    Testbench

--- top.sv
+++ top.sv
@@ -1,62 +1,57 @@
-module top_cp2;
+module top;
 
 bit CLK = '0;
-bit MNMX = '1;
-bit TEST = '1;
-bit RESET = '0;
-bit READY = '1;
-bit NMI = '0;
-bit INTR = '0;
-bit HOLD = '0;
+bit RESET = 0;
 
-wire logic [7:0] AD;
-logic [19:8] A;
-logic HLDA;
-logic IOM;
-logic WR;
-logic RD;
-logic SSO;
-logic INTA;
-logic ALE;
-logic DTR;
-logic DEN;
+initial
+	begin
+		Bus.MNMX = '1;
+		Bus.TEST = '1;
+		Bus.READY = '1;
+		Bus.NMI = '0;
+		Bus.INTR = '0;
+		Bus.HOLD = '0;
+	end
 
 logic [19:0] Address;
 wire logic [7:0]  Data;
 
 bit [3:0] CS;
 
-Intel8088 P(CLK, MNMX, TEST, RESET, READY, NMI, INTR, HOLD, AD, A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN);
+// Creating an instance for our interface
+Intel8088Pins Bus(.CLK(CLK),.RESET(RESET));
 
-io_memory DUT1(.CLK(CLK),.RESET(RESET),.CS(CS[0]),.ALE(ALE),.RD(RD),.WR(WR),.Address(Address),.Data(Data));
-io_memory DUT2(.CLK(CLK),.RESET(RESET),.CS(CS[1]),.ALE(ALE),.RD(RD),.WR(WR),.Address(Address),.Data(Data));
-io_memory DUT3(.CLK(CLK),.RESET(RESET),.CS(CS[2]),.ALE(ALE),.RD(RD),.WR(WR),.Address(Address),.Data(Data));
-io_memory DUT4(.CLK(CLK),.RESET(RESET),.CS(CS[3]),.ALE(ALE),.RD(RD),.WR(WR),.Address(Address),.Data(Data));
+Intel8088 P(Bus.Processor);
+
+io_memory #(.addr_bits(20),.data_bits(8),.trace(9)) Mhigh(Bus.Peripheral, Address, Data, CS[0]);
+io_memory #(.addr_bits(20),.data_bits(8),.trace(2)) Mlow(Bus.Peripheral, Address, Data, CS[1]);
+io_memory #(.addr_bits(16),.data_bits(8),.trace(3)) IO1(Bus.Peripheral, Address, Data, CS[2]);
+io_memory #(.addr_bits(16),.data_bits(8),.trace(4)) IO2(Bus.Peripheral, Address, Data, CS[3]);
 
 // 8282 Latch to latch bus address
 always_latch
 begin
-if (ALE)
-	Address <= {A, AD};
+if (Bus.ALE)
+	Address <= {Bus.A, Bus.AD};
 end
 
 // 8286 transceiver
-assign Data =  (DTR & ~DEN) ? AD   : 'z;
-assign AD   = (~DTR & ~DEN) ? Data : 'z;
+assign Data =  (Bus.DTR & ~Bus.DEN) ? Bus.AD   : 'z;
+assign Bus.AD   = (~Bus.DTR & ~Bus.DEN) ? Data : 'z;
 
 //Chip Select logic
 always_comb
 begin
-	if(!IOM && Address[19])
+	if(!Bus.IOM && Address[19])
 		CS = 4'b0001;
 		
-	else if(!IOM && !Address[19])
+	else if(!Bus.IOM && !Address[19])
 		CS = 4'b0010;
 		
-	else if(IOM && Address[15:4] == 12'hFF0)
+	else if(Bus.IOM && Address[15:4] == 12'hFF0)
 		CS = 4'b0100;
 		
-	else if(IOM && Address[15:9] == 8'h0E)
+	else if(Bus.IOM && Address[15:9] == 8'h0E)
 		CS = 4'b1000;
 	
 	else
